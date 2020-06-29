516422ca822f6b2e673bc03963e4623ff20f056f
Ryan Robinson <smallbedroomstudios@gmail.com>
2020-06-29 23:05:43 +0100

    finished basic homepage, responsiveness is okay

--- src/Components/Cards/CardTight.js
+++ src/Components/Cards/CardTight.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import testImage from "../../logo.svg";
 import lmsImage from "../../Components/Main/Homepage/last-man-standing.png";
 
-function Cards() {
+function CardsTight() {
   return (
     <div className={`${classes.card}`}>
       <div
@@ -14,17 +14,15 @@ function Cards() {
         ${classes.padding_all_25px} 
         ${classes.border_thin_radius_6px} 
         ${classes.center_align}
-        ${classes.box_shadow_1px}
-        ${classes.bg_color}
         `}
       >
         <img className={`${classes.img_full}`} src={lmsImage}></img>
         <div
           className={`${classes.border_bottom_1px} ${classes.margin_top_15px}`}
         ></div>
-        <h1>Title: Card</h1>
+        <h1>Title: Card Tight</h1>
         <p>
-          description: n one column card, this is being controlled by the main
+          description: one column card, this is being controlled by the main
           grid container which we have set to be 1 column, we will need to
           change this later
         </p>
@@ -36,15 +34,7 @@ function Cards() {
         </Link>
       </div>
     </div>
-
-    // <div className={`${classes.container}`}>
-    //   <div className={`${classes.row} `}>
-    //     <div
-    //       className={`${classes.col_4} ${classes.border_thin_radius_6px}`}
-    //     ></div>
-    //   </div>
-    // </div>
   );
 }
 
-export default Cards;
+export default CardsTight;
--- src/Components/Main/Homepage/Homepage.js
+++ src/Components/Main/Homepage/Homepage.js
@@ -3,6 +3,7 @@ import classes from "./Homepage.module.scss";
 import lmsImage from "./last-man-standing.png";
 import Cards from "../../Cards/Cards";
 import { Link } from "react-router-dom";
+import CardsTight from "../../Cards/CardTight";
 
 const howToPlayText = "How to play?";
 const howToPlayDesc = `It’s easy to set up a league and play Football Survivor. As
@@ -50,15 +51,19 @@ function Homepage() {
         </div>
       </div> */}
 
-      <div className={`${classes.container} ${classes.container_max_width_60}`}>
-        <div className={`${classes.row} ${classes.grid_gap_10px}`}>
+      <div
+        className={`${classes.container} ${classes.container_max_width_1280px} ${classes.margin_all_10px}`}
+      >
+        <div
+          className={`${classes.row} ${classes.grid_gap_10px} ${classes.margin_y_25px} ${classes.margin_all_10px}`} //extra 10 margin to align with the cards
+        >
           <div
-            className={`${classes.col_4} ${classes.margin_y_25px} ${classes.border_thin_radius_10px}`}
+            className={`${classes.col_4} ${classes.border_thin_radius_10px}`}
           >
             <h1> This will be in the intro / pitch text.</h1>
             <p> Quick introduction to what the product is</p>
           </div>
-          <div className={`${classes.col_8} ${classes.margin_y_25px}`}>
+          <div className={`${classes.col_8}`}>
             <img
               className={`${classes.img_full} ${classes.img_restrict_height_512px} ${classes.img_radius_10px} `}
               src={lmsImage}
@@ -74,9 +79,7 @@ function Homepage() {
           </div>
         </div>
 
-        <div
-          className={`${classes.row} ${classes.grid_gap_10px} ${classes.center_align}`}
-        >
+        <div className={`${classes.row} ${classes.center_align}`}>
           <div className={classes.col_4}>
             <h1>How To Play: 1</h1>
             <Cards></Cards>
@@ -94,12 +97,12 @@ function Homepage() {
         </div>
 
         <div
-          className={`${classes.row} ${classes.grid_gap_10px} ${classes.center_align}`}
+          className={`${classes.row} ${classes.grid_gap_10px} ${classes.center_align} ${classes.margin_top_50px} `}
         >
-          <div className={classes.col_12}>
+          <div className={`${classes.col_12} ${classes.margin_bottom_50px}`}>
             <h1>Fancy Creating Your Own League?</h1>
             <Link
-              className={`${classes.btn} ${classes.primary} ${classes.button_width_70pc} ${classes.button_center}`}
+              className={`${classes.btn} ${classes.primary} ${classes.button_width_50pc} ${classes.button_center}`}
               to="#"
             >
               Create A League
@@ -108,10 +111,14 @@ function Homepage() {
         </div>
 
         <div
-          className={`${classes.row} ${classes.grid_gap_10px} ${classes.center_align}`}
+          className={`${classes.row} ${classes.center_align} ${classes.border_thin_radius_10px} ${classes.box_shadow_1px} ${classes.margin_all_10px}`}
         >
-          <div className={classes.col_12}>
-            <h1>Leagues</h1>
+          <div className={classes.col_6}>
+            <h1>Open Leagues</h1>
+            <Cards></Cards>
+          </div>
+          <div className={classes.col_6}>
+            <h1>Closed Leagues</h1>
             <Cards></Cards>
           </div>
         </div>
